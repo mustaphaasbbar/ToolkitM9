2594fbc4035d01252b09bb80577f4cdb6286c775
squabbi <squabbik@gmail.com>
2015-03-20 19:28:43 +1100

    added fastboot flash window

---
+++ ToolkitM9/Flash.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ToolkitM9
+{
+    /// <summary>
+    /// Interaction logic for Flash.xaml
+    /// </summary>
+    public partial class Flash : Window
+    {
+        public Flash()
+        {
+            InitializeComponent();
+        }
+    }
+}
